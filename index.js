e42b953e12c40ce292fb5c0cb0ddd365f70824c9
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-06-19 22:14:34 -0400

    Add a comment to app.listen in index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 
 
+
+
+// Start a web server and listen to the connections on the specified local host
 app.listen(3000, () => {
     console.log('App is running on local host 3000!!!')
 })
